737d2ddbf99a9ebbf38f4de463430ff352981b32
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-07-31 18:36:25 +0200

    chore: Refactor components and update dependencies

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../components/HomePage.vue";
-import PostDetail from "../components/PostDetail.vue";
+import HomePage from "../views/HomePage.vue";
+import PostDetail from "../views/PostDetail.vue";
 
 const routes = [
    { path: "/", component: HomePage },
