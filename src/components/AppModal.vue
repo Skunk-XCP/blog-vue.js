65eff95c54cafd711ecf010f9c1ed9fbd06f1ff6
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-09-26 11:21:06 +0200

    Refactor user display and adjust styles in AppModal and UserDashboard views

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,39 +1,39 @@
 <template>
-   <div
-      v-if="isVisible"
-      class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
-   >
-      <div class="bg-white p-4 rounded shadow-lg">
-         <p>{{ message }}</p>
-         <div class="flex justify-end mt-4">
-            <button
-               @click="$emit('cancel')"
-               class="bg-gray-500 text-white px-4 py-2 rounded mr-2"
-            >
-               Annuler
-            </button>
-            <button
-               @click="$emit('confirm')"
-               class="bg-red-500 text-white px-4 py-2 rounded"
-            >
-               Confirmer
-            </button>
-         </div>
+  <div
+    v-if="isVisible"
+    class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+  >
+    <div class="bg-white p-4 rounded shadow-lg">
+      <p>{{ message }}</p>
+      <div class="flex justify-end mt-4">
+        <button
+          @click="$emit('cancel')"
+          class="text-black px-4 py-2 mr-2 font-bold"
+        >
+          Annuler
+        </button>
+        <button
+          @click="$emit('confirm')"
+          class="text-red-500 px-4 py-2 font-bold"
+        >
+          Confirmer
+        </button>
       </div>
-   </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   props: {
-      isVisible: {
-         type: Boolean,
-         required: true,
-      },
-      message: {
-         type: String,
-         required: true,
-      },
-   },
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,49 +1,53 @@
 <template>
-   <div class="p-10 max-w-3xl mx-auto">
-      <h1 class="text-3xl font-bold mb-6">Tableau de Bord</h1>
-      <button
-         @click="createNewPost"
-         class="mb-6 bg-green-500 text-white p-2 rounded"
+  <div class="p-10 max-w-3xl mx-auto">
+    <h1 class="text-3xl font-bold mb-6">Tableau de Bord</h1>
+    <button
+      @click="createNewPost"
+      class="mb-6 bg-green-500 text-white p-2 rounded"
+    >
+      Créer un nouvel article
+    </button>
+    <div v-if="posts.length === 0">
+      <p>Aucun article disponible.</p>
+    </div>
+    <div v-else>
+      <div
+        v-for="post in posts"
+        :key="post.id"
+        class="mb-10 border p-4 rounded"
       >
-         Créer un nouvel article
-      </button>
-      <div v-if="posts.length === 0">
-         <p>Aucun article disponible.</p>
+        <h2 class="text-2xl font-bold">{{ post.title }}</h2>
+        <p>{{ post.content.substring(0, 100) }}...</p>
+        <div class="flex space-x-4 mt-8">
+          <button
+            @click="previewPost(post.id)"
+            class="px-4 py-2 font-bold underline"
+          >
+            Aperçu
+          </button>
+          <button
+            @click="editPost(post.id)"
+            class="px-4 py-2 font-bold underline"
+          >
+            Modifier
+          </button>
+          <button
+            @click="showDeleteConfirmation(post.id, post.title)"
+            class="text-red-500 px-4 py-2 font-bold underline"
+          >
+            Supprimer
+          </button>
+        </div>
       </div>
-      <div v-else>
-         <div v-for="post in posts" :key="post.id" class="mb-10">
-            <h2 class="text-2xl font-bold">{{ post.title }}</h2>
-            <p>{{ post.content.substring(0, 100) }}...</p>
-            <div class="flex space-x-4 mt-2">
-               <button
-                  @click="previewPost(post.id)"
-                  class="bg-gray-500 text-white px-4 py-2 rounded"
-               >
-                  Aperçu
-               </button>
-               <button
-                  @click="editPost(post.id)"
-                  class="bg-blue-500 text-white px-4 py-2 rounded"
-               >
-                  Modifier
-               </button>
-               <button
-                  @click="showDeleteConfirmation(post.id)"
-                  class="bg-red-500 text-white px-4 py-2 rounded"
-               >
-                  Supprimer
-               </button>
-            </div>
-         </div>
-      </div>
-      <AppModal
-         v-if="isModalVisible"
-         :isVisible="isModalVisible"
-         message="Êtes-vous sûr de vouloir supprimer cet article ?"
-         @confirm="confirmDelete"
-         @cancel="cancelDelete"
-      />
-   </div>
+    </div>
+    <AppModal
+      v-if="isModalVisible"
+      :isVisible="isModalVisible"
+      :message="`Êtes-vous sûr de vouloir supprimer l'article '${postTitleToDelete}' ?`"
+      @confirm="confirmDelete"
+      @cancel="cancelDelete"
+    />
+  </div>
 </template>
 
 <script>
@@ -54,79 +58,84 @@ import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
-   components: {
-      AppModal,
-   },
-   setup() {
-      const posts = ref([]);
-      const error = ref(null);
-      const router = useRouter();
-      const isModalVisible = ref(false);
-      const postIdToDelete = ref(null);
+  components: {
+    AppModal,
+  },
+  setup() {
+    const posts = ref([]);
+    const error = ref(null);
+    const router = useRouter();
+    const isModalVisible = ref(false);
+    const postIdToDelete = ref(null);
+    const postTitleToDelete = ref("");
 
-      const fetchPosts = async () => {
-         try {
-            const {
-               data: { user },
-            } = await supabase.auth.getUser();
-            if (user) {
-               posts.value = await fetchPostsByUserId(user.id);
-            } else {
-               router.push("/login");
-            }
-         } catch (err) {
-            error.value = "Erreur lors de la récupération des articles.";
-            console.error("Erreur lors de la récupération des articles :", err);
-         }
-      };
+    const fetchPosts = async () => {
+      try {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+        if (user) {
+          posts.value = await fetchPostsByUserId(user.id);
+        } else {
+          router.push("/login");
+        }
+      } catch (err) {
+        error.value = "Erreur lors de la récupération des articles.";
+        console.error("Erreur lors de la récupération des articles :", err);
+      }
+    };
 
-      const editPost = (postId) => {
-         router.push(`/edit/${postId}`);
-      };
+    const editPost = (postId) => {
+      router.push(`/edit/${postId}`);
+    };
 
-      const createNewPost = () => {
-         router.push("/create");
-      };
+    const createNewPost = () => {
+      router.push("/create");
+    };
 
-      const previewPost = (postId) => {
-         router.push(`/preview/${postId}`);
-      };
+    const previewPost = (postId) => {
+      router.push(`/preview/${postId}`);
+    };
 
-      const showDeleteConfirmation = (postId) => {
-         postIdToDelete.value = postId;
-         isModalVisible.value = true;
-      };
+    const showDeleteConfirmation = (postId, postTitle) => {
+      postIdToDelete.value = postId;
+      postTitleToDelete.value = postTitle;
+      isModalVisible.value = true;
+    };
 
-      const confirmDelete = async () => {
-         try {
-            await deletePost(postIdToDelete.value);
-            posts.value = posts.value.filter(
-               (post) => post.id !== postIdToDelete.value
-            );
-            isModalVisible.value = false;
-            postIdToDelete.value = null;
-         } catch (err) {
-            console.error("Erreur lors de la suppression de l'article :", err);
-         }
-      };
+    const confirmDelete = async () => {
+      try {
+        await deletePost(postIdToDelete.value);
+        posts.value = posts.value.filter(
+          (post) => post.id !== postIdToDelete.value
+        );
+        isModalVisible.value = false;
+        postIdToDelete.value = null;
+        postTitleToDelete.value = "";
+      } catch (err) {
+        console.error("Erreur lors de la suppression de l'article :", err);
+      }
+    };
 
-      const cancelDelete = () => {
-         isModalVisible.value = false;
-         postIdToDelete.value = null;
-      };
+    const cancelDelete = () => {
+      isModalVisible.value = false;
+      postIdToDelete.value = null;
+      postTitleToDelete.value = "";
+    };
 
-      onMounted(fetchPosts);
+    onMounted(fetchPosts);
 
-      return {
-         posts,
-         editPost,
-         createNewPost,
-         showDeleteConfirmation,
-         confirmDelete,
-         cancelDelete,
-         isModalVisible,
-         previewPost,
-      };
-   },
+    return {
+      posts,
+      editPost,
+      createNewPost,
+      showDeleteConfirmation,
+      confirmDelete,
+      cancelDelete,
+      isModalVisible,
+      previewPost,
+      postTitleToDelete,
+    };
+  },
 };
 </script>
